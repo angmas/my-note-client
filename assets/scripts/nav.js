314cf8bf561b88b2df3851395036b08a8d86380a
angmas <angela.mashlan@safeco.com>
2017-04-30 21:47:26 -0400

    Added code to have sign-up modal appear
    -added addHandler function to nav.js to attach events that have
    to do with displaying pages
    -renamed the landingPage function to showLandingPage
    -added the showSignUpModal function
    -added attributes to the sign-up button a tag in the landing-page
    handlebar to work with triggering the display of the sign-up modal
    -added attributes to the sign-up-modal handlebar to have the modal
    be triggered by the sign-up button.

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,23 +1,32 @@
 'use strict'
 const showLandingPageTemplate = require('./templates/landing-page.handlebars')
 
-const authEvents = require('./auth/events.js')
+const signUpModal = require('./templates/sign-up-modal.handlebars')
+
+// const authEvents = require('./auth/events.js')
 
 let firstLoad = true
 
-const landingPage = function () {
+const showSignUpModal = function () {
+  console.log('I am in signUpModal')
+  $('.container').append(signUpModal)
+}
+const showLandingPage = function () {
   $('.container').append(showLandingPageTemplate)
-  authEvents.addHandlers()
+  addHandlers()
 }
 const doFirstLoad = function () {
   console.log('I am in doFirstLoad')
   if (firstLoad === true) {
-    landingPage()
+    showLandingPage()
     firstLoad = false
   }
+}
+const addHandlers = function () {
+  $('#sign-up-btn').on('click', showSignUpModal)
 }
 
 module.exports = {
   doFirstLoad,
-  landingPage
+  showLandingPage
 }
