ca5e541fdaf4656fbdc393bdafcf66c3af7fe9bb
angmas <angela.mashlan@safeco.com>
2017-04-30 21:10:05 -0400

    Added code to click on sign-up button and hear click event.
    - added an id to the landing-page sign up button
    - added a call to addHandlers function for auth events
    - copied over auth folder from the api-auth-token repo
    - attached a click event to the sign-up-button with a console
    log when the onSignUp function was reached successfully

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,10 +1,13 @@
 'use strict'
 const showLandingPageTemplate = require('./templates/landing-page.handlebars')
 
+const authEvents = require('./auth/events.js')
+
 let firstLoad = true
 
 const landingPage = function () {
   $('.container').append(showLandingPageTemplate)
+  authEvents.addHandlers()
 }
 const doFirstLoad = function () {
   console.log('I am in doFirstLoad')
