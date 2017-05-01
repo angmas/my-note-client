9f2730a48add9dafbe11ebcb00953ed80ffc2a45
angmas <angela.mashlan@safeco.com>
2017-05-01 13:34:52 -0400

    Added functionality to display the create new note modal.
    -added the note-create-modal handlebar
    -updated the home handlebar so that the button toggles the modal
    -updated the navPages code with the showCreateNoteModal function
    to append the note-create-modal handlebar

--- assets/scripts/navPages.js
+++ assets/scripts/navPages.js
@@ -1,21 +1,31 @@
 'use strict'
 const store = require('./store')
 const showHomeTemplate = require('./templates/home.handlebars')
+const showNoteCreateModalTemplate = require('./templates/note-create-modal.handlebars')
 
 const clearModal = function () {
   $('.modal').modal('hide')
+  $('form').trigger('reset')
   $('.modal').modal('backdrop')
   $('.container').empty()
 }
 const showHomePage = function () {
   clearModal()
   $('.container').append(showHomeTemplate({ email: store.user.email }))
+  addHandlers()
+}
+
+const showNoteCreateModal = function () {
+  $('.container').append(showNoteCreateModalTemplate)
   // addHandlers()
 }
 
-// const addHandlers = function () {
-//   $('#sign-up-btn').on('click', showSignUpModal)
-// }
+const addHandlers = function () {
+  console.log('am in navPages/addHandlers')
+  $('#note-create-btn').on('click', showNoteCreateModal)
+}
 
 module.exports = {
-  showHomePage}
+  showHomePage,
+  showNoteCreateModal
+}
