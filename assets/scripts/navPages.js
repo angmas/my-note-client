896d65c34185aa5ff8460d00e7230902aad22a48
angmas <angela.mashlan@safeco.com>
2017-05-02 12:41:08 -0400

    Added code to support displaying the note title in delete confirmation modal
    -added span tags to make finding the text easier though jQuery
    to confirm-delete-modal template and the note-listing template
    -added function to extract the note title from the elements
    -added function to add the note title to the modal

--- assets/scripts/navPages.js
+++ assets/scripts/navPages.js
@@ -15,10 +15,41 @@ const showHomePage = function () {
   $('.container').append(showHomeTemplate({ email: store.user.email,
     notes: store.notes }))
   noteEvents.addHandlers()
+  addHandlers()
 }
 const addNoteToView = function () {
   clearModal()
   $('.container').append(oneNoteTemplate({ notes: store.note }))
+}
+
+const passTitleToModal = function () {
+  const title = parseTitle(this)
+  console.log('I am in passTitleModal title: ', title)
+  $('.modal-body #delete-title').text(title)
+}
+
+const parseTitle = function (element) {
+  let title
+  console.log('parseTitle this: ', element)
+  const note = $(element).parent()
+  const noteSpan = $(note).children('span')
+  const strong = $(note).children('strong')
+  const strongSpan = $(strong).children('span')
+
+  if (strong.length === 0) {
+    title = $(noteSpan).text()
+  } else {
+    title = $(strongSpan).text()
+    title.trimLeft()
+  }
+  return title
+  // console.log('In passTitleToModal span.length: ', noteSpan.length)
+  // console.log('In passTitleToModal strong.length:', strong.length)
+  // console.log('In passTitleModal title: ', title)
+}
+
+const addHandlers = function () {
+  $('.btn-remove-class').on('click', passTitleToModal)
 }
 
 module.exports = {
