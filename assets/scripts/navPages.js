32b1d1125ad8f6f4a89f3d6a0eefc69f67d8c452
angmas <angela.mashlan@safeco.com>
2017-05-02 13:56:26 -0400

    Added code to get data-id of note item and pass pass as attribute to modal.

--- assets/scripts/navPages.js
+++ assets/scripts/navPages.js
@@ -22,15 +22,17 @@ const addNoteToView = function () {
   $('.container').append(oneNoteTemplate({ notes: store.note }))
 }
 
-const passTitleToModal = function () {
-  const title = parseTitle(this)
-  console.log('I am in passTitleModal title: ', title)
-  $('.modal-body #delete-title').text(title)
+const getDataId = function (element) {
+  const li = $(element).parent()
+  const dataId = $(li).attr('data-id')
+  return dataId
 }
-
 const parseTitle = function (element) {
   let title
   console.log('parseTitle this: ', element)
+  // The note title will exist in span element.
+  // If the note item favorite=true, then the
+  // the span will be a child of the strong element
   const note = $(element).parent()
   const noteSpan = $(note).children('span')
   const strong = $(note).children('strong')
@@ -47,9 +49,16 @@ const parseTitle = function (element) {
   // console.log('In passTitleToModal strong.length:', strong.length)
   // console.log('In passTitleModal title: ', title)
 }
-
+const passDataToModal = function () {
+  const title = parseTitle(this)
+  const dataId = getDataId(this)
+  console.log('I am in passDataModal title: ', title)
+  console.log('I am in passDataModal dataId: ', dataId)
+  $('.modal-body #delete-title').text(title)
+  $('#confirm-delete-modal').attr('data-id', dataId)
+}
 const addHandlers = function () {
-  $('.btn-remove-class').on('click', passTitleToModal)
+  $('.btn-remove-class').on('click', passDataToModal)
 }
 
 module.exports = {
