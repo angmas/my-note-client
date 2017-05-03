c0f1c40b87f61e405771373b0fee1b90bf03a8cf
angmas <angela.mashlan@safeco.com>
2017-05-03 04:19:54 -0400

    Cleaned up notes/ui.js to remove functions that were moved to NavPages.js

--- assets/scripts/note/ui.js
+++ assets/scripts/note/ui.js
@@ -1,50 +1,23 @@
 'use strict'
-const store = require('../store')
-// const api = require('./api')
-
-const navPages = require('../navPages')
-
-const createNoteSuccess = (data) => {
-  console.log('createNoteSuccess ran: ', data)
-}
 
 const createNoteFailure = (error) => {
   console.error('createNoteFailure ran: ', error)
 }
 
-const showNotesSuccess = (data) => {
-  store.notes = data.user.notes
-  console.log('showNotesSuccess ran: ', store)
-  navPages.showHomePage()
-}
-
 const showNotesFailure = (error) => {
   console.error('showNotesFailure ran:', error)
-}
-const updateNoteSuccess = () => {
-  console.log('updateNoteSuccess ran')
-  store.user = null
 }
 
 const updateNoteFailure = (error) => {
   console.error('updateNoteFailure ran:', error)
 }
 
-const changePasswordSuccess = () => {
-  console.log('changePasswordSuccess ran')
+const destroyNoteFailure = (error) => {
+  console.error('updateNoteFailure ran:', error)
 }
-
-const changePasswordFailure = (error) => {
-  console.error('changePasswordFailure ran:', error)
-}
-
 module.exports = {
-  createNoteSuccess,
   createNoteFailure,
-  showNotesSuccess,
   showNotesFailure,
-  updateNoteSuccess,
   updateNoteFailure,
-  changePasswordSuccess,
-  changePasswordFailure
+  destroyNoteFailure
 }
