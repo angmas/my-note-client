d81b398500365d6f55406e6bff940f0f1989510e
angmas <angela.mashlan@safeco.com>
2017-05-15 00:30:11 -0400

    Remove helper console logs

--- assets/scripts/helper.js
+++ assets/scripts/helper.js
@@ -1,9 +1,9 @@
 'use strict'
 const getDataId = function (element) {
   const modal = $(element).closest('.modal')
-  console.log('I am in getDataId element: ', modal)
+  // console.log('I am in getDataId element: ', modal)
   const dataId = $(modal).attr('data-id')
-  console.log('Iam in getDataId dataId: ', dataId)
+  // console.log('Iam in getDataId dataId: ', dataId)
   return dataId
 }
 
